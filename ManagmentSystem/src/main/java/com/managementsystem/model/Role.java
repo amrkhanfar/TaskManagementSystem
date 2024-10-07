6b48e06bf3db4ccc0d7bf2390e3acd8a37e87607
amrkhanfar <ultraakch@gmail.com>
2024-10-07 14:59:35 +0300

    Applied java conventions and documeneted com.managementsystem.model

--- ManagmentSystem/src/main/java/com/managementsystem/model/Role.java
+++ ManagmentSystem/src/main/java/com/managementsystem/model/Role.java
@@ -1,40 +1,49 @@
 package com.managementsystem.model;
 
+/**
+ * Represents a role in the Task Management System.
+ * 
+ */
 public class Role {
-	
-	private int id;
-	private String role_name;
-	
-	public static int MANAGER = 1;
-	public static int TEAM_LEADER = 2;
-	public static int DEVELOPER = 3;
-	
-	
-	public Role(int id, String role_name) {
-		super();
-		this.id = id;
-		this.role_name = role_name;
-	}
+    
+    
+    /** Constant representing the Manager role ID in the database. */
+    public static int MANAGER = 1;
+    
+    /** Constant representing the Team Leader role ID in the database. */
+    public static int TEAM_LEADER = 2;
+    
+    /** Constant representing the Developer role ID in the database. */
+    public static int DEVELOPER = 3;
+    private int id;
+    private String role_name;
+    
+    /**
+     * Constructs a new Role with the specified parameters.
+     * 
+     * @param id         The ID of the role.
+     * @param role_name  The name of the role.
+     */
+    public Role(int id, String role_name) {
+	super();
+	this.id = id;
+	this.role_name = role_name;
+    }
 
+    public int getId() {
+	return id;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public void setId(int id) {
+	this.id = id;
+    }
 
+    public String getRole_name() {
+	return role_name;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setRole_name(String role_name) {
+	this.role_name = role_name;
+    }
 
-
-	public String getRole_name() {
-		return role_name;
-	}
-
-
-	public void setRole_name(String role_name) {
-		this.role_name = role_name;
-	}
-	
-	
 }
--- ManagmentSystem/src/main/java/com/managementsystem/model/Team.java
+++ ManagmentSystem/src/main/java/com/managementsystem/model/Team.java
@@ -1,47 +1,53 @@
 package com.managementsystem.model;
 
+/**
+ * 
+* Team - Represents a team in the Task Management System.
+*
+* @author Amr
+* @version Oct 7, 2024
+ */
 public class Team {
-	
-	private int id;
-	private String team_name;
-	private int team_leader_id;
-	
-	
-	public Team(int id, String team_name, int team_leader_id) {
-		this.id = id;
-		this.team_name = team_name;
-		this.team_leader_id = team_leader_id;
-	}
 
+    private int id;
+    private String team_name;
+    private int team_leader_id;
+    
+    /**
+     * Constructs a new Team with the specified parameters
+     * 
+     * @param id              The ID of the team.
+     * @param team_name       The name of the team.
+     * @param team_leader_id  The ID of the team leader.
+     */
+    public Team(int id, String team_name, int team_leader_id) {
+	this.id = id;
+	this.team_name = team_name;
+	this.team_leader_id = team_leader_id;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+	return id;
+    }
 
+    public void setId(int id) {
+	this.id = id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public String getTeam_name() {
+	return team_name;
+    }
 
+    public void setTeam_name(String team_name) {
+	this.team_name = team_name;
+    }
 
-	public String getTeam_name() {
-		return team_name;
-	}
+    public int getTeam_leader_id() {
+	return team_leader_id;
+    }
 
+    public void setTeam_leader_id(int team_leader_id) {
+	this.team_leader_id = team_leader_id;
+    }
 
-	public void setTeam_name(String team_name) {
-		this.team_name = team_name;
-	}
-
-
-	public int getTeam_leader_id() {
-		return team_leader_id;
-	}
-
-
-	public void setTeam_leader_id(int team_leader_id) {
-		this.team_leader_id = team_leader_id;
-	}
-	
-	
 }
